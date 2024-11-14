c9fb86e23fc68dd9fe0418ae4f7901d464bb2406
phani kumar <phanikumarmeejuru@gmail.com>
2024-11-14 18:17:39 +0530

    Initial commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from app.forms import *
 from django.http import HttpResponse
+from django.core.mail import send_mail
 # Create your views here.
 
 def display(request):
@@ -16,12 +17,18 @@ def display(request):
             pw=NMUFDO.cleaned_data['password']
             MUFDO.set_password(pw)
             MUFDO.save()
-
             MPFDO=NMPFDO.save(commit=False)
             MPFDO.username=MUFDO
             MPFDO.save()
+            send_mail('Registration',
+            'Regsitartion is Successfulll',
+            'phanikumarmeejuru@gmail.com',
+            [MUFDO.email],
+            fail_silently=False)
 
             return HttpResponse('REgistration is Successfull')
         else:
+            print(NMUFDO.errors)  # Check errors
+            print(NMPFDO.errors)  # Check errors
             return HttpResponse('Invalid Data')
     return render(request,'display.html',d)
--- num8/settings.py
+++ num8/settings.py
@@ -124,7 +124,16 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'phanikumarmeejuru@gmail.com'
+EMAIL_HOST_PASSWORD = 'zeitanjziuxixovi'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_PORT = 587
